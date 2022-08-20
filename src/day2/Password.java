cc58f92714f56d4f3af8e401cb58d61b91b73a44
Sviatlana Andreyenka <s.andreenko66@gmail.com>
2022-08-20 18:39:57 -0400

    Password file is created

--- src/day2/Password.java
+++ src/day2/Password.java
@@ -3,5 +3,8 @@ package day2;
 public class Password {
     public static void main(String[] args) {
         System.out.println("Create a password");
+
+        //create some changes
+        //commit
     }
 }
